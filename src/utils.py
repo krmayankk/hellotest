c04bf959fcaff61c3042aeb479ac5ab3dc58137b
Mayank <krmayankk@gmail.com>
2026-04-18 01:10:14 -0700

    refactor: rename calculate_total to compute_total with tax support
    Added tax_rate parameter. Also added Stripe and webhook config keys.

--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,8 @@
-def calculate_total(price, quantity, discount=0):
-    """Calculate total price after discount."""
+def compute_total(price, quantity, tax_rate=0, discount=0):
+    """Compute total price with tax and discount."""
     subtotal = price * quantity
-    return subtotal - (subtotal * discount)
+    after_discount = subtotal - (subtotal * discount)
+    return after_discount + (after_discount * tax_rate)
 
 
 def format_currency(amount):
